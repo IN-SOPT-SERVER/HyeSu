9ec356db8adbb7fac3927396df4fe188ca495799
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-10-08 16:30:28 +0900

    [Add] 2차세미나코드

---
+++ seminar02/code/callback.ts
@@ -0,0 +1,13 @@
+console.log("Ready...");
+
+setTimeout((): void => {
+    console.log("Set ...");
+}, 3000);
+
+console.log("Go~! ^^");
+
+// Ready
+// Go
+// Set
+
+/// -> 이렇게 비동기 처리를 할 수 있다. (근데 잘 안쓴다)
