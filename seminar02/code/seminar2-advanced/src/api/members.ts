d2e1b1d78519d9ca9c62d62397d464f8a421b178
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-10-12 03:09:11 +0900

    [Feat] 멤버 이름으로 개별름 조회 추가

--- seminar02/code/seminar2-advanced/src/api/members.ts
+++ seminar02/code/seminar2-advanced/src/api/members.ts
@@ -34,9 +34,30 @@ const members: Member[] = [
 router.get("/", (req: Request, res: Response) => {
     return res.status(statusCode.OK).json({
         status: statusCode.OK,
-        message: "멤버 조회 성공",
+        message: "멤버 전체 조회 성공",
         data: members
     });
+});
+
+router.get("/:name", (req: Request, res: Response) => {
+    const name = req.params.name;
+
+    const member = members.find((member) => {
+        return member.name === name
+    });
+
+    if (member === undefined) {
+        return res.status(statusCode.NOT_FOUND).json({
+            statusCode: statusCode.NOT_FOUND,
+            message: `${name}이라는 분은 안계셔요`
+        })
+    }
+
+    return res.status(statusCode.OK).json({
+        status: statusCode.OK,
+        message: "멤버 이름으로 조회 성공",
+        data: member
+    }); 
 });
 
 module.exports = router;
